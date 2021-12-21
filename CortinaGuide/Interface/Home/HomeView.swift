ba9fb45e6088df46f47a33b43ee8434dc281a08d
Mariam <mariam.obaji@student.nhlstenden.com>
2021-12-21 15:50:14 +0100

    [FEATURE] Added some UI

--- CortinaGuide/Interface/Home/HomeView.swift
+++ CortinaGuide/Interface/Home/HomeView.swift
@@ -16,12 +16,59 @@ struct HomeView: View {
     weak var delegate: HomeViewDelegate?
     
     var body: some View {
-        VStack {
-            Button(action: {
-                delegate?.navigateToAR()
-            }, label: {
-                Text("Show IN AR")
-            })
+        ZStack {
+//            Color.purple
+//                .ignoresSafeArea()
+            VStack {
+                
+                Spacer ()
+                Text ("Fiets in AR")
+                    .font(.title)
+                    .fontWeight(.black)
+                    .padding(.bottom)
+                
+                
+                Text ("Augmented Reality room..")
+                    .padding(.bottom, 100)
+                
+                
+                HStack {
+                    Circle ()
+                        .fill (Color.black)
+                        .frame (width:20, height:15)
+                        .padding(.bottom, 50)
+                    
+                    Circle ()
+                        .strokeBorder (Color.black, lineWidth: 2)
+                        .background(Circle() .fill (Color.white))
+                        .frame (width:20, height:15)
+                        .padding(.bottom, 50)
+                    
+                    Circle ()
+                        .strokeBorder (Color.black, lineWidth: 2)
+                        .background(Circle() .fill (Color.white))
+                        .frame (width:20, height:15)
+                        .padding(.bottom, 50)
+                
+                }
+                
+                   
+                    Button(action: {
+                        delegate?.navigateToAR()
+                    }, label: {
+                        Text("Volgende")
+                            .fontWeight(.heavy)
+                            .foregroundColor(Color.black)
+                    })
+                        .frame (width: 150, height:60)
+                        .background (Color.white)
+                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 2)
+
+                
+                Spacer ()
+                
+            }
+//            .background(.purple)
         }
     }
 }
